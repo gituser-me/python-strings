c305c42b48fe8fe286a865583792a6622f84450c
gituser-me <76775113+gituser-me@users.noreply.github.com>
2021-02-09 21:19:06 +0530

    Create create-string.py

---
+++ create-string.py
@@ -0,0 +1,10 @@
+# creating our first Python string
+
+# strings are basically pieces of information 
+# which are enclosed by single quotation
+# or double quotation marks
+string1 = "Hello"
+string2 = "I love Python"
+
+print(string1) # outputs: Hello
+print(string2) # outputs: I love Python
